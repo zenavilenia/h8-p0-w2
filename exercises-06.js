aa0fde8b54de7340b7e5f62ad4c7bf264794872e
Zena <zena.vilenia@gmail.com>
2018-02-01 13:05:58 +0700

    edit spasi

--- exercises-06.js
+++ exercises-06.js
@@ -13,6 +13,8 @@ while(number >= 2) {
   number-=2;
 }
 
+console.log();
+
 // ===========================================
 
 // 2. Melakukan Looping Menggunakan For
@@ -26,41 +28,49 @@ for(var i = 20; i >= 1; i--) {
   console.log(i + " - I will become fullstack developer");
 }
 
+console.log();
+
 // ============================================
 
 // 3. Angka Ganjil dan Genap
 // Ganjil genap
 for(var i = 1; i <= 100; i++) {
-  if(i%2 == 0) {
+  if(i%2 === 0) {
     console.log("GENAP");
   } else {
     console.log("GANJIL");
   }
 }
 
+console.log();
+
 // Pertambahan counter 2
 for(var i = 1; i <= 100; i+=2) {
-  if(i%3 == 0) {
+  if(i%3 === 0) {
     console.log(i + " KELIPATAN 3");
   } else {
-    console.log("");
+    console.log();
   }
 }
+
+console.log();
 
 // Pertambahan counter 5
 for(var i = 1; i <= 100; i+=5) {
-  if(i%6 == 0) {
+  if(i%6 === 0) {
     console.log(i + " KELIPATAN 6");
   } else {
-    console.log("");
+    console.log();
   }
 }
 
+console.log();
+
 // Pertambahan counter 9
 for(var i = 1; i <= 100; i+=9) {
-  if(i%10 == 0) {
+  if(i%10 === 0) {
     console.log(i + " KELIPATAN 10");
   } else {
-    console.log("");
+    console.log();
   }
 }
