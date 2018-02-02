aad1feafb61e8ff3f0aa9344fdc49c2cb5da2a3f
Zena <zena.vilenia@gmail.com>
2018-02-02 22:29:52 +0700

    edit no 1

--- exercises-05.js
+++ exercises-05.js
@@ -8,8 +8,9 @@ var fifth = 'I';
 var sixth = 'love';
 var seventh = 'it!';
 
-console.log(word + " " + second + " " + third + " " +
-           fourth + " " + fifth + " " + sixth + " " + seventh);
+word = word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh;
+
+console.log(word);
 
 // ==============================================================
 
